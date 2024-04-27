0ccb3069137b720a57f0b05d8c322389775eec97
Miguel Peixe Aldeias <hello@migloo.be>
2024-04-27 21:29:20 +0200

    chore: remove first name & last name in favour of username

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,16 @@
-// import { db } from "astro:db";
+import { db, User } from "astro:db";
+import { Argon2id } from "oslo/password";
+
+import { newId } from "@/lib/utils";
 
 // ? https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+	const hashedPassword = await new Argon2id().hash("password");
+
+	await db.insert(User).values({
+		id: newId("user"),
+		email: "admin@astro-cms.com",
+		username: "Admin",
+		hashedPassword,
+	});
 }
